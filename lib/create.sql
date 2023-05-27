5287a71aede0a8d0b7dce64116b9a05c4ce7d5e0
sunga-lee <sungajunior1@gmail.com>
2023-05-27 15:06:27 +0300

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    / / columns here id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    colour TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 7, 'M', 'Red', 'Sanguine', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 10, 'F', 'White', 'Melancholic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 3, 'M', 'Brown', 'Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 2, 'F', 'Pink', 'Phlegmatic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 15, 'M', 'Grey', 'Melancholic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 5, 'F', 'Purple', 'Sanguine', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 5, 'F', 'Blue', 'Phlegmatic', 1);
